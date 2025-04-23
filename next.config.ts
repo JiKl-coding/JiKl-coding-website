841f626ab605f7357a910906f55365787d996b7c
JiKl Coding <klatovskyjiri@gmail.com>
2025-04-23 21:50:50 +0200

    revert

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['cs', 'en'],
-    defaultLocale: 'cs',
-    localeDetection: false,
-  },
+
 }
 
 export default nextConfig
