10dcb4f73010b996b149cdeb81463373ecd900a0
JiKl Coding <klatovskyjiri@gmail.com>
2025-04-24 12:51:28 +0200

    me text

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -8,7 +8,7 @@ export const translations = {
     about: {
       title: "O mně",
       description:
-        "Vyvíjím podnikovou aplikaci pro pojišťovnu v prostředí Progress OpenEdge (ABL). Ve volném čase ale rád tvořím moderní webové stránky a neustále se učím nové technologie – aktuálně se zaměřuji na Next.js, Tailwind a TypeScript. Mimo CS se věnuji především sportu a hudbě, studuji stoicismus a věnuji se svému pejskovi. V případě zájmu mě kontaktujte a můžeme se domluvit na spolupráci.",
+        "Vyvíjím podnikovou aplikaci pro pojišťovnu v prostředí Progress OpenEdge (ABL). Ve volném čase ale rád tvořím moderní webové stránky, vymýšlím zajímavé desgigny, a neustále se učím nové technologie – aktuálně se zaměřuji na Next.js, Tailwind a TypeScript. Mimo CS se věnuji především sportu a hudbě, studuji stoicismus a věnuji se svému pejskovi. V případě zájmu mě kontaktujte a můžeme se domluvit na spolupráci.",
     },
     me: "já",
     skills: "dovednosti",
@@ -27,7 +27,7 @@ export const translations = {
     about: {
       title: "About Me",
       description:
-        "I develop an enterprise application for an insurance company using Progress OpenEdge (ABL). In my free time, I enjoy building modern websites and continuously learning new technologies – currently focusing on Next.js, Tailwind, and TypeScript. Outside of computer science, I dedicate myself mainly to sports and music, I study stoicism, and take care of my dog. If you're interested, feel free to contact me and we can discuss working together."
+        "I develop an enterprise application for an insurance company using Progress OpenEdge (ABL). In my free time, I enjoy creating modern websites, coming up with interesting designs, and continuously learning new technologies – currently focusing on Next.js, Tailwind, and TypeScript. Outside of computer science, I dedicate myself mainly to sports and music, I study stoicism, and take care of my dog. If you're interested, feel free to contact me and we can discuss working together."
     },
     me: "me",
     skills: "skills",
