6e90e26a27fadd8f9ee18fc3cb8ea1d14b4df54f
JiKl Coding <klatovskyjiri@gmail.com>
2025-04-24 20:51:07 +0200

    hotfix mailu

--- components/partials/Footer.tsx
+++ components/partials/Footer.tsx
@@ -38,7 +38,7 @@ const Footer = ({ locale }: { locale: "cs" | "en" }) => {
             <FaInstagram />
           </a>
           <a
-            href="mailto:jikl-coding@gmail.com"
+            href="mailto:jikl.coding@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Email"
